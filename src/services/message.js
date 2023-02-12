e629c25c73c049f3a84fc1b7623806328a426db5
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2023-02-12 14:13:10 -0500

    Some changes

--- src/services/message.js
+++ src/services/message.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseURL = 'http://localhost:3000/api'
+const baseURL = 'simple-clone-chatgpt-backend-production.up.railway.app/api'
 
 export const getMessageByGPT = async (data) => {
   const response = await axios.post(`${baseURL}/prompt`, data);
